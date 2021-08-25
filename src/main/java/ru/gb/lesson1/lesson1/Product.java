3b2f65ff4bcf0dc0f21c693bdcef7374208557bf
obelix <sonicx921@yandex.ru>
2021-08-25 13:42:27 +0300

    Первое задание

---
+++ src/main/java/ru/gb/lesson1/lesson1/Product.java
@@ -0,0 +1,38 @@
+package ru.gb.lesson1.lesson1;
+
+public class Product {
+    private int id;
+    private String title;
+    //упростим, а данном случае точность не нужна
+    private float cost;
+
+    public Product(int id, String title, float cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+}
